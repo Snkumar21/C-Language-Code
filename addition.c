96e7ce29b87b41c0a10da2facf82d6439676bae1
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-23 00:26:48 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &a);
+
+    printf("Enter another number: ");
+    scanf("%d", &b);
+
+    result = a + b;
+
+    printf("The result of %d + %d is: %d\n", a, b, result);
+
+    return 0;
+}
